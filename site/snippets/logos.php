13236edec034f1d2d4d75306b580ec251f803f67
Nathan Uphoff <nathan@Nathan.local>
2017-05-01 16:58:13 +0200

    fixed logo party

--- site/snippets/logos.php
+++ site/snippets/logos.php
@@ -1,15 +1,9 @@
 <ul class="logo__party">
   
-  <li class="item"><a href="https://ec.europa.eu/programmes/horizon2020/"><img src="<?php echo url('assets/images/logo-eu.svg') ?>" /></a></li>
-  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/logo-making-sense.svg') ?>" /></a></li>
-  <li class="item"><a href="https://www.cinekid.nl/"><img src="<?php echo url('assets/images/logo-cinekid.svg') ?>" /></a></li>
-  <li class="item"><a href="http://stichtingsec.nl/"><img src="<?php echo url('assets/images/logo-stichting-educatie-en-cultuur.svg') ?>" /></a></li>
-  <li class="item"><a href="http://www.rivm.nl/"><img src="<?php echo url('assets/images/logo-rivm.svg') ?>" /></a></li>
-
-  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/logo-sidn-fonds.svg') ?>" /></a></li>
-  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/logo-fonds-21.svg') ?>" /></a></li>
-  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/logo-bits-of-freedom.svg') ?>" /></a></li>
-  <li class="item"><a href="http://waag.org/"><img src="<?php echo url('assets/images/logo-waag-society.svg') ?>" /></a></li>
-  <li class="item"><a href="http://www.amsterdam.nl/"><img src="<?php echo url('assets/images/logo-netwerk-democratie.svg') ?>" /></a></li>
+  <li class="item"><a href="https://www.sidnfonds.nl/"><img src="<?php echo url('assets/images/logo-sidn-fonds.svg') ?>" /></a></li>
+  <li class="item"><a href="https://www.fonds21.nl/"><img src="<?php echo url('assets/images/logo-fonds-21.svg') ?>" /></a></li>
+  <li class="item"><a href="https://www.bof.nl/"><img src="<?php echo url('assets/images/logo-bits-of-freedom.svg') ?>" /></a></li>
+  <li class="item"><a href="https://waag.org/"><img src="<?php echo url('assets/images/logo-waag-society.svg') ?>" /></a></li>
+  <li class="item"><a href="http://netdem.nl/"><img src="<?php echo url('assets/images/logo-netwerk-democratie.svg') ?>" /></a></li>
 
 </ul>
